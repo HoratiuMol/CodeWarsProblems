414da3684b6f61f16004100fd849bd10ef9eacfe
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-07-18 01:09:07 +0200

    Da error de OutofBoundsException

---
+++ src/PasamosMétodos.java
@@ -0,0 +1,6 @@
+public class PasamosMétodos extends UnflattenAList_Easy {
+    public static void main(String[] args) {
+        int [] flatArray= new int[]{1, 2, 3, 5, 4, 2, 4, 3, 6};
+        unflatten(flatArray);
+    }
+}
--- src/UnflattenAList_Easy.java
+++ src/UnflattenAList_Easy.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class UnflattenAList_Easy {
+public class UnflattenAList_Easy {
+            public static Object[] unflatten(int[] flatArray)
+            {
+                //flatArray= new int[]{1, 2, 3, 5, 4, 2, 4, 3, 6};
+
+                Object[] objeto= new Object[]{};
+
+                for(int i=0; i<flatArray.length;){
+
+                    if(flatArray[i]<3){
+                        objeto[i]=flatArray[i];
+                        i++;
+                    }else{
+
+                        int tama=flatArray[i];
+
+                        int[] int1=new int[]{tama};
+
+                        for(int j=0;j<tama;j++){
+
+                            int1[j]=flatArray[i];
+                            i++;
+                        }
+
+                        objeto[i]=int1;
+                        //i=i+tama;
+
+                    }
+
+                }
+
+                return objeto;
+
+    }
 }
