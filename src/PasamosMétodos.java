5157b364a4e840c3e9abea5e5bb513c70d7806a1
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-07-20 00:27:37 +0200

    Válido, pero en CodeWars es tipo JavaScript, yo lo hice tipo Java

--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -1,4 +1,4 @@
-public class PasamosMétodos extends TwoSum{
+public class PasamosMétodos extends BouncingBall{
    /* public static void main(String[] args) {
         int [] flatArray= new int[]{1, 2, 3, 5, 4, 2, 4, 3, 6};
         unflatten(flatArray);
@@ -6,9 +6,13 @@ public class PasamosMétodos extends TwoSum{
     Hecho
     */
 
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
         //int [] passArray =new int[]{1,4,5,2,4,5,6,23,6,6,7,4,10};
         int [] passArray =new int[]{2,2,3};
         twoSum(passArray,4);
+    }*/
+
+    public static void main(String[] args) {
+        BouncingBall(40,0.9);
     }
 }
