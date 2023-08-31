4f3870ce11e5644303d4b917b1eae87b7ab6a9d9
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-08-31 21:11:07 +0200

    Hay que revisar esto en el codewards https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/java

---
+++ src/YoureASquare.java
@@ -0,0 +1,10 @@
+import static java.lang.Math.*;
+
+public class YoureASquare {
+    public static double isSquare(int n) {
+
+        sqrt(n);
+        System.out.println(sqrt(n));
+        return sqrt(n); // fix me!
+    }
+}
