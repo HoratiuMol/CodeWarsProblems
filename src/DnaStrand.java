bef01c85fccdb8dfa17cf01c24eb1e5656a8aebb
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-03 00:27:21 +0200

    Hay que revisar esto en el codewards https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/java

---
+++ src/DnaStrand.java
@@ -0,0 +1,21 @@
+public class DnaStrand {
+    public static String makeComplement(String dna) {
+        char[] oldchar=dna.toCharArray();
+        System.out.println(oldchar);
+
+        for(int i=0;i<oldchar.length;i++){
+
+            if(oldchar[i]=="A"){
+                oldchar[i]="T";
+            }else if(oldchar[i]=="T"){
+                oldchar[i]="A";
+            }else if(oldchar[i]=="G"){
+                oldchar[i]="C";
+            }else if(oldchar[i]=="C"){
+                oldchar[i]="G";
+            }
+
+        }
+        return String.valueOf(oldchar);;
+    }
+}
--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -1,8 +1,13 @@
 import java.math.BigInteger;
 
-public class PasamosMétodos extends YoureASquare{
+public class PasamosMétodos extends DnaStrand{
+    public PasamosMétodos(String dna) {
+        super(dna);
+    }
+
     public static void main(String[] args) {
-        isSquare(4);
+        String dna="CTAT";
+        new DnaStrand("CTAT");
     }
 
 }
