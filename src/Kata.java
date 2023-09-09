04efa722ec149b20def8ade4e460a802219d65fb
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-09 18:56:04 +0200

    Perf

--- src/Kata.java
+++ src/Kata.java
@@ -15,4 +15,8 @@ For example, for [1, 2, 2] it should return 9 because 1^2+2^2+2^2=9
         System.out.println(result);
         return result;//Your Code
     }
+    public static String numberToString(int num) {
+        // Return a string of the number here!
+        return String.valueOf(num);
+    }
 }
