3019e5c6a907a3067be64bff526e4e6be6dc5df3
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-10 20:36:30 +0200

    6º Kyu

--- src/Kata.java
+++ src/Kata.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Kata {
     /*Complete the square sum function so that it squares each number passed into it and then sums the results together.
 
@@ -18,5 +20,26 @@ For example, for [1, 2, 2] it should return 9 because 1^2+2^2+2^2=9
     public static String numberToString(int num) {
         // Return a string of the number here!
         return String.valueOf(num);
+    }
+
+    public static double areaOfPolygonInsideCircle(double circleRadius, int numberOfSides) {
+        //opcion 1: area 1 triángulo por N triángulos
+        double ladoEqui,areaT, areaTotal,ladoequiDF,areaTDF;
+        DecimalFormat df= new DecimalFormat("####.###");
+//(nr2/2) sin (2π/n) square units.
+        double sen;
+        sen=Math.sin(2*Math.PI/numberOfSides);
+        double sup;
+
+
+        sup=((numberOfSides*(circleRadius*circleRadius))/2)*sen;
+        System.out.println("seno :" + sen);
+        System.out.println("superficie :"+ sup);
+        df.format(sup);
+        System.out.println("superfi :" + df.format(sup));
+        //S=(1/2)*N*r^2*sen(360/N)
+
+        return Double.parseDouble(df.format(sup));
+
     }
 }
--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -1,11 +1,12 @@
 import java.math.BigInteger;
 import java.sql.Array;
 
-public class PasamosMétodos extends Positive{
+public class PasamosMétodos extends Kata{
 
     public static void main(String[] args) {
-        int[] db={};
-        sum(db);
+        double radio=2;
+        int nSides=4 ;
+        areaOfPolygonInsideCircle(radio,nSides);
     }
 
 
