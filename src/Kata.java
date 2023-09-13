8e69b1f2d5e199a47b77a28a5e6c03ccfbaac8d7
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-13 21:42:59 +0200

    revisar bien https://www.codewars.com/kata/5f0ed36164f2bc00283aed07/train/java

--- src/Kata.java
+++ src/Kata.java
@@ -192,4 +192,57 @@ if bmi > 30 return "Obese"
 
         return bmiS;
     }
+    public static long overTheRoad(long address, long n) {
+        /*
+        Task
+You've just moved into a perfectly straight s
+treet with exactly n identical houses on either side of the road.
+ Naturally, you would like to find out the house number of the people on the other side of the street.
+ The street looks something like this:
+
+Street
+1|   |6
+3|   |4
+5|   |2
+  you
+
+Evens increase on the right; odds decrease on the left. House numbers start at 1 and increase without gaps. When n = 3, 1 is opposite 6, 3 opposite 4, and 5 opposite 2.
+         Example (address, n --> output)
+Given your house number address and length of street n, give the house number on the opposite side of the street.
+
+1, 3 --> 6
+3, 3 --> 4
+2, 3 --> 5
+3, 5 --> 8
+Note about errors
+If you are timing out, running out of memory, or get any kind of "error", read on.
+Both n and address could get upto 500 billion with over 200 random tests. If you try to store the addresses of 500
+billion houses in a list then you will run out of memory and the tests will crash.
+This is not a kata problem so please don't post an issue. Similarly if the tests don't complete
+ within 12 seconds then you also fail.
+
+To solve this, you need to think of a way to do the kata without making massive lists or huge for loops. Read the discourse for some inspiration :)
+
+         */
+        long izq=1, drc=n*2, drcT=2,total=n*2;
+        long fin=2;
+
+        for(int i=1;i<address;i++){
+
+            izq=izq+2;
+            drc=drc-2;
+            drcT=drcT+2;
+
+
+        }
+
+
+        System.out.println("izq último es: " + izq);
+        System.out.println("derecha ultimo: " + total);
+        System.out.println("drc primero es: " + drc);
+
+
+        return drc;
+    }
+
 }
--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -5,9 +5,8 @@ public class PasamosMétodos extends Kata{
 
     public static void main(String[] args) {
         double weight,height;
-        weight=80;
-        height=1.2;
-        bmi(weight,height);
+        long adress=2, n=3;
+        overTheRoad(adress,n);
     }
 
 
