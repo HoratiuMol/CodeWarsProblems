15129f4c7e75621badaa37a3223086da82ffa8fd
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-23 12:44:47 +0200

    perfecto

--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -1,13 +1,18 @@
+import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.sql.Array;
+import java.util.ArrayList;
 
 public class PasamosMétodos extends Kata2{
 
     public static void main(String[] args) {
 
-        int a=39;
-        double[] b=new double[]{4,13,14};
-        tribonacci(b,a);
+        ArrayList<int[]> arr=new ArrayList<>();
+        arr.add(new int[]{10,0});
+        arr.add(new int[]{0,3});
+        arr.add(new int[]{6,1});
+        arr.add(new int[]{5,2});
+        countPassengers(arr);
+
     }
 
 
