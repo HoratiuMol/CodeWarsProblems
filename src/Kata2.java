0f35f4e66a060c4fb4fc03f567cf0d4621d3978b
HoratiuMol <horatiu.moldovan93@gmail.com>
2023-09-23 13:17:07 +0200

    perfecto

--- src/Kata2.java
+++ src/Kata2.java
@@ -200,5 +200,29 @@ If you enjoyed this kata more advanced and generalized version of it can be foun
         return enBus;
     }
 
+    public static boolean setAlarm(boolean employed, boolean vacation) {
+        /*
+        Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters.
+        The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm).
+It should return false otherwise. Examples:
+
+employed | vacation
+true     | true     => false
+true     | false    => true
+false    | true     => false
+false    | false    => false
+         */
+
+        if(employed==false){
+            return false;
+        } else if (employed==true && vacation==false){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 
 }
--- src/PasamosMétodos.java
+++ src/PasamosMétodos.java
@@ -6,12 +6,8 @@ public class PasamosMétodos extends Kata2{
 
     public static void main(String[] args) {
 
-        ArrayList<int[]> arr=new ArrayList<>();
-        arr.add(new int[]{10,0});
-        arr.add(new int[]{0,3});
-        arr.add(new int[]{6,1});
-        arr.add(new int[]{5,2});
-        countPassengers(arr);
+
+        setAlarm(true, false);
 
     }
 
